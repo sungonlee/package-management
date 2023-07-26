201609e0764b56c549fb7e6c8029bdd688a5be30
sungon.lee <sungon.lee@encored.co.jp>
2023-07-26 14:40:17 +0900

    init

---
+++ setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+def take_package_name(name):
+    if name.startswith("-e"):
+        return name[name.find("=")+1:name.rfind("-")]
+    else:
+        return name.strip()
+
+
+def load_requires_from_file(filepath):
+    with open(filepath) as fp:
+        return [take_package_name(pkg_name) for pkg_name in fp.readlines()]
+
+
+def load_links_from_file(filepath):
+    res = []
+    with open(filepath) as fp:
+        for pkg_name in fp.readlines():
+            if pkg_name.startswith("-e"):
+                res.append(pkg_name.split(" ")[1])
+    return res
+
+
+setuptools.setup(
+    name="ejhelper",
+    version="0.0.1",
+    author="sungon",
+    author_email="sungon.lee@encored.co.jp",
+    description="ejhelper is lib for encored japan development",
+    url="https://encored.co.jp",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    include_package_data=True,
+    python_requires=">=3.7",
+)
+
+git+https://github.com/encoredjapan/encored-helper-py.git#egg=ejhelper
